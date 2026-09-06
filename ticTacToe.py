b8bea408ce8246ecb4df008def9b22fb048f1c1f
Adam <aabutaleb@worcester.edu>
2026-09-06 15:50:13 -0400

    The game now alternates turns between two players, handles bad inputs, and ends the game in a draw if the board has no playable squares left. TODO: -Finish the method that checks if someone has won and achieved three pieces in a row -When the game ends as a win or a draw, give the players the choice to play again

--- ticTacToe.py
+++ ticTacToe.py
@@ -27,35 +27,73 @@ def print_board(board):
 class player:
     def __init__(self, player_piece):
         self.player_piece = player_piece
-    """""
-    def get_move(self):
-        y = input("Enter your move's Y coordinate: ")
-        x = input("Enter your move's X coordinate: ")
-        game_board[x][y] = self.player_piece
-        print_board(game_board)
-    """
 
 def get_move(player_piece):
-    X = int(input("Enter your move's X coordinate: "))
-    Y = int(input("Enter your move's Y coordinate: "))
-    execute_move(X, Y, player_piece)
+    while True:
+        if player_piece == 'X':
+            print("Player one: ")
+        else :
+            print("Player two: ")
+        try:
+            X = int(input("Enter your move's X coordinate: "))
+            Y = int(input("Enter your move's Y coordinate: "))
+        except ValueError: #handles non-int inputs
+            print("That is not a valid coordinate. Please enter digits 1-3 only.")
+            #reprint the game board
+            print_board(game_board)
+            continue
+
+        if X not in (1, 2, 3) or Y not in (1, 2, 3): #handles int inputs outside the range of the board
+            print("That is not a valid coordinate. Please enter digits 1-3 only.")
+            #reprint the game board
+            print_board(game_board)
+            continue
+
+        if game_board[Y-1][X-1] != '-': #handles input where there is already a piece
+            print("That square is already taken.")
+            #reprint the game board
+            print_board(game_board)
+            continue
+
+        execute_move(X, Y, player_piece)
+        break
 
 def execute_move(X, Y, player_piece):
     game_board[Y-1][X-1] = player_piece
     print_board(game_board)
 
-player1 = player("X")
-player2 = player("O")
+def is_board_full(board):
+    for row in board:
+        for col in row:
+            if col == '-':
+                return False
+    else:
+        return True
 
-print("Welcome to Tic-Tac-Toe")
-print("Player one will use the 'X' piece")
-print("Player two will use the 'O' piece")
-print_board(game_board)
+def get_winner(board):
+    pass
 
-#test case
-get_move(player1.player_piece)
+#tic-tac-toe keeps playing until; the board has no more spaces, or there is three in a row
+#I will use a while loop that will run until either of these conditions are met
+
+def play_game():
+    player1 = player("X")
+    player2 = player("O")
+    print("Welcome to Tic-Tac-Toe")
+    print("Player one will use the 'X' piece")
+    print("Player two will use the 'O' piece")
+    print_board(game_board)
+    player_turn = 0
+    while True:
+        if player_turn == 0:
+            get_move(player1.player_piece)
+            player_turn = 1
+        else:
+            get_move(player2.player_piece)
+            player_turn = 0
+        if is_board_full(game_board):
+            print("IT'S A DRAW!")
+            break
 
 
-
-
-
+play_game()
